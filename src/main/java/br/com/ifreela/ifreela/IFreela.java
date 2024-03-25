e7f52c9f13549bf75c35eaf4904867c860050b53
Victor Emanuel <emanuel.victor1313@gmail.com>
2024-03-25 20:35:45 -0300

    feat(ifreela) : adiciona tela de login

---
+++ src/main/java/br/com/ifreela/ifreela/IFreela.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifreela.ifreela;
+
+/**
+ *
+ * @author 82329522
+ */
+public class IFreela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
