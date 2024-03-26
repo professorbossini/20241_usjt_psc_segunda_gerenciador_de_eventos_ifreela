4b6cf16264c47c35911c14afdaf4040453b3b46e
Rodrigo Bossini <professorbossini@gmail.com>
2024-03-25 21:40:56 -0300

    feat(ifreela): cria tabela usuario no banco

---
+++ script.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tb_usuario(
+	cod_usuario SERIAL PRIMARY KEY,
+	login VARCHAR(200),
+	senha VARCHAR(200),
+	tipo_usuario INT
+);
+
+DROP TABLE tb_usuario;
+
+INSERT INTO tb_usuario 
+(login, senha, tipo_usuario)
+VALUES ('admin', 'admin', 1);
+
+SELECT * FROM tb_usuario;
